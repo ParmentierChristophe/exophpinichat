237f70aec10c68ad34c8393b12ff553b2541bad8
Christophe Parmentier <christophe-parmentier@laposte.net>
2016-10-17 13:56:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 try
 {
-   $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', 'simplon');
+   $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', '');
 }
 catch(Exception $e)
 {
