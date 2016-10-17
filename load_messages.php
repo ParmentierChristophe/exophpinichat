513748458aaac244d3bcbaee28d2d515bd03109b
Christophe Parmentier <christophe-parmentier@laposte.net>
2016-10-17 13:56:36 +0200

    Update load_messages.php

--- load_messages.php
+++ load_messages.php
@@ -16,7 +16,7 @@
 
 try
 {
-   $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', 'simplon');
+   $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', '');
 }
 catch(Exception $e)
 {
